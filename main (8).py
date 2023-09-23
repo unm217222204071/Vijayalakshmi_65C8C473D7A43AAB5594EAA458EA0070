afa5b1cbff475d00c2e56abb228a87f0adeaf0bf
unm217222204071 <144506565+unm217222204071@users.noreply.github.com>
2023-09-23 16:48:48 +0530

    Add files via upload

---
+++ main (8).py
@@ -0,0 +1,12 @@
+def isLeapyear(year):
+ if(year %4==0 and year%100!=0)or year%400==0:
+   return True
+ else:
+   return False
+
+
+year=2014
+if isLeapyear(year):
+  print ('{}is a leap year'.format(year))
+else:
+  print('{}is not a leap year'.format(year))
